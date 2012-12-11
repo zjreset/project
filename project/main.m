9bdc34a4f9ff07cbac27973ff8bee70e072bfa9a
runes <runes@runes-MacBook-Pro.local>
2012-12-11 17:04:46 +0800

    Initial Commit

---
+++ project/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  project
+//
+//  Created by runes on 12-12-11.
+//  Copyright (c) 2012年 runes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
